a9b6bb1e6176785861f1aecb66347442f8632aaf
shanebasham <shanebasham28@gmail.com>
2023-09-23 23:54:29 -0600

    Assignment

---
+++ cse121b/scripts/w02-task.js
@@ -0,0 +1,32 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName = 'Shane Basham';
+let currentYear = '2023';
+let profilePicture = 'images/me.jpg';
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.getElementById('#year');
+const imageElement = document.getElementById('image');
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = '<strong>Shane Basham</strong>';
+element.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+
+let food = ['Cherries', 'Cherry Pie', 'Cherry Cheese Cake', 'Cherry Ice Cream']
+
+foodElement.innerHTML += `<br>${favFoods}`;
+
+
+
