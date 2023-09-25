0aac432ed10a2176f0970f60696172f897ebfb96
shanebasham <shanebasham28@gmail.com>
2023-09-25 13:21:56 -0600

    fixed week 1 error 2

--- cse121b/Week 1/w01-task.js
+++ cse121b/Week 1/w01-task.js
@@ -5,11 +5,10 @@ userName = "Moronihah";
 console.log(`Username: ${userName}`);
 
 // 🔍 Part 2 error
-let currentDateAndTime = DateTime;
+let currentDateAndTime = (new Date());
 console.log(`It is now ${currentDateAndTime}`);
 
 // 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments.
-
 let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
