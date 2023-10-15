c37d3d374b1bd55443cc6582ab953ca3f75e7370
shanebasham <shanebasham28@gmail.com>
2023-10-14 23:35:46 -0600

    week 5

---
+++ cse121b/scripts/w05-task.js
@@ -0,0 +1,24 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.getElementById('temples');
+let templeList = [];
+
+/* async displayTemples Function */
+const displayTemples = (temples) => {
+
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* sortBy Function */
+
+
+
+getTemples();
+
+/* Event Listener */
