70de6d9a43943cbb099d70cc641ff5aba3eabe8a
shanebasham <shanebasham28@gmail.com>
2023-10-24 16:46:25 -0600

    project edits1

--- cse121b/scripts/projecttest2.js
+++ cse121b/scripts/projecttest2.js
@@ -20,8 +20,8 @@ const displayRecipes = (recipes) => {
 /* async getRecipes Function using fetch()*/
 const getRecipes = async () => {
     const response = await fetch("https://shanebasham.github.io/PythonMessingAround/cse121b/scripts/recipes.json");
-    templeList = await response.json();
-    displayRecipes(templeList);
+    recipeList = await response.json();
+    displayRecipes(recipeList);
 };
 
 /* reset Function */
@@ -29,23 +29,27 @@ const reset = () => {
     recipesElement.innerHTML = '';
 };
 
-/* sortBy Function */
-const sortBy = (recipes) => {
-    reset();
-    switch (document.querySelector('#sortBy').value) {
-        case 'utah':
-            displayRecipes(recipes.filter(temple => temple.location.includes('Utah')));
-            break;
-        case 'notutah':
-            displayRecipes(recipes.filter(temple => !temple.location.includes('Utah')));
-            break;
-        case 'older':
-            displayRecipes(recipes.filter(temple => new Date(temple.dedicatedDate) < new Date(1950, 0, 1)));
-            break;
-        case 'all':
-            displayRecipes(recipes);
-            break;
-    };
-}
+// /* sortBy Function */
+// const sortBy = (recipes) => {
+//     reset();
+//     switch (document.querySelector('#sortBy').value) {
+//         case 'utah':
+//             displayRecipes(recipes.filter(temple => temple.location.includes('Utah')));
+//             break;
+//         case 'notutah':
+//             displayRecipes(recipes.filter(temple => !temple.location.includes('Utah')));
+//             break;
+//         case 'older':
+//             displayRecipes(recipes.filter(temple => new Date(temple.dedicatedDate) < new Date(1950, 0, 1)));
+//             break;
+//         case 'all':
+//             displayRecipes(recipes);
+//             break;
+//     };
+// }
 
 getRecipes();
+
+
+// /* Event Listener */
+// document.querySelector('#sortBy').addEventListener('change', () => { sortBy(recipeList) });
